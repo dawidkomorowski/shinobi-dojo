5de8adea7f2538f82f6c186f25049068825c2846
Dawid Komorowski <dawidkomorowski@outlook.com>
2020-12-04 01:43:57 +0100

    Created empty solution.

---
+++ ShinobiDojo/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ShinobiDojo
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+        }
+    }
+}
